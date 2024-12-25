380f88590da03f182d245a2f6b7ee8d00f11fee2
albnnc <albnnc@gmail.com>
2024-12-25 20:14:57 +0300

    w

--- utils/parse_structure/mod_test.ts
+++ utils/parse_structure/mod_test.ts
@@ -1,3 +1,4 @@
+import { assertEquals } from "@std/assert/equals";
 import * as path from "@std/path";
 import { getStructureByteArrays } from "../get_structure_byte_arrays.ts";
 import { parseStructure } from "./mod.ts";
@@ -8,5 +9,11 @@ Deno.test("parse structure", async () => {
   );
   const structureByteArrays = getStructureByteArrays(bytes);
   const structures = structureByteArrays.map(parseStructure);
-  console.log(structures);
+  assertEquals(structures.filter((v) => v.type === "BIOS").length, 1);
+  assertEquals(structures.filter((v) => v.type === "SYSTEM").length, 1);
+  assertEquals(structures.filter((v) => v.type === "BASEBOARD").length, 1);
+  assertEquals(structures.filter((v) => v.type === "CHASSIS").length, 1);
+  assertEquals(structures.filter((v) => v.type === "PROCESSOR").length, 1);
+  assertEquals(structures.filter((v) => v.type === "MEMORY_DEVICE").length, 4);
+  // TODO: Add better checks.
 });
