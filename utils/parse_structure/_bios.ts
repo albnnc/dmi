5631ecae7e1d251f9599468744ce49ed028aca4d
albnnc <albnnc@gmail.com>
2024-12-27 12:24:43 +0300

    w

--- utils/parse_structure/_bios.ts
+++ utils/parse_structure/_bios.ts
@@ -11,12 +11,20 @@ export function parseBiosStructure(bytes: number[]): Structure {
   })();
   const vendor = strings[bytes[4] - 1];
   const version = strings[bytes[5] - 1];
+  const biosStartingAddressSegment = (() => {
+    const dataView = new DataView(new ArrayBuffer(2));
+    dataView.setUint8(0, bytes[6]);
+    dataView.setUint8(1, bytes[7]);
+    // 0x0000 means unknown value.
+    return dataView.getUint16(0, true) || undefined;
+  })();
   const releaseDate = strings[bytes[8] - 1];
   return {
     type: "BIOS" as const,
     handle,
     vendor,
     version,
+    biosStartingAddressSegment,
     releaseDate,
   };
 }
